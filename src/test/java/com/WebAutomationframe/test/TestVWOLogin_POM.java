0bc64f66fbb477a006eff9176cee28825a6a1485
amolkatke <katkeamol16@gmail.com>
2024-08-26 15:11:29 +0530

    Pom

--- src/test/java/com/WebAutomationframe/test/TestVWOLogin_POM.java
+++ src/test/java/com/WebAutomationframe/test/TestVWOLogin_POM.java
@@ -16,11 +16,12 @@ public class TestVWOLogin_POM {
         driver.quit();
     }
     @Test
-    public void testLoginNegativeVWO2(){
+    public void testLoginNegativeVWO2() throws InterruptedException {
         WebDriver driver = new EdgeDriver();
         LoginPage_POM loginPagePom = new LoginPage_POM(driver);
         String error_msg_text = loginPagePom.loginToVWOInvalidCreds("pramod@gmail.com","admin");
         Assert.assertEquals(error_msg_text,"Your email, password, IP address or location did not match");
+        Thread.sleep(4000);
         driver.quit();
     }
 
